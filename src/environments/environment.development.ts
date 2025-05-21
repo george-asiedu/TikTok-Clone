e282d76806c19f2ee8d57c8757c38d35517a2a75
george-asiedu <asiedug41@gmail.com>
2025-05-21 21:43:39 +0000

    auth url added to env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,7 @@
 export const environment = {
-    production: false,
-    rapidApiUrl: 'https://tiktok-api23.p.rapidapi.com/api',
-    rapidApiKey: 'c295efc728msh5eb912dc524a64cp1f169ajsnbf1e4b674ee4',
-    rapidApiHost: 'tiktok-api23.p.rapidapi.com',
-  };
+  production: false,
+  authUrl: 'https://authentication-rejd.onrender.com/api/',
+  rapidApiUrl: 'https://tiktok-api23.p.rapidapi.com/api',
+  rapidApiKey: 'c295efc728msh5eb912dc524a64cp1f169ajsnbf1e4b674ee4',
+  rapidApiHost: 'tiktok-api23.p.rapidapi.com',
+};
