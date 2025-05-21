956fef2781407f947f63d943b6eacf9fb2803587
george-asiedu <asiedug41@gmail.com>
2025-05-21 23:00:45 +0000

    toast position field defined

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgToastModule, ToasterPosition } from 'ng-angular-popup';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgToastModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  
+  public toastPosition = ToasterPosition.TOP_RIGHT;
 }
