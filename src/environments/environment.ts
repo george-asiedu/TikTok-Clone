616afeee8beaf9c38b9dc3ee80cd24980e60d6a8
george-asiedu <asiedug41@gmail.com>
2025-05-21 20:28:19 +0000

    angular project setup

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    rapidApiUrl: 'https://tiktok-api23.p.rapidapi.com/api',
+    rapidApiKey: 'c295efc728msh5eb912dc524a64cp1f169ajsnbf1e4b674ee4',
+    rapidApiHost: 'tiktok-api23.p.rapidapi.com',
+  };
