ecce37d256a17ed05af3115e430ea6864d7c2140
george-asiedu <asiedug41@gmail.com>
2025-05-21 21:59:06 +0000

    auth interface defined

---
+++ src/app/interface/auth.ts
@@ -0,0 +1,53 @@
+export interface Signup {
+    email: string;
+    password: string;
+    name: string;
+    phone: string;
+    dob: string;
+}
+
+export interface Login {
+    email?: string;
+    phone?: string;
+    password: string;
+}
+
+export interface VerifyAccount {
+    code: string;
+}
+
+export interface SignupResponse {
+    message: string;
+    data: {
+        user: {
+            id: number;
+            name: string;
+            email: string;
+            role: string;
+            isVerified: boolean;
+            dob: string;
+            phone: string;
+        };
+        token: string;
+    };
+}
+
+export interface LoginResponse {
+    message: string;
+    data: {
+        user: {
+            id: number;
+            name: string;
+            email: string;
+            role: string;
+            isVerified: boolean;
+            dob: string;
+            phone: string;
+        };
+        token: string;
+    };
+}
+
+export interface SuccessResponse {
+    message: string;
+}
