0bde27a88c86f091f51822c5cd33f12279867074
george-asiedu <asiedug41@gmail.com>
2025-05-22 15:27:29 +0000

    auth actions defined

---
+++ src/app/auth/state/auth.actions.ts
@@ -0,0 +1,24 @@
+import {createActionGroup, emptyProps, props} from "@ngrx/store";
+import {
+    Login,
+    LoginResponse,
+    Signup,
+    SignupResponse,
+    SuccessResponse,
+    VerifyAccount
+} from "../../interface/auth";
+import {HttpErrorResponse} from "@angular/common/http";
+
+export const authActions = createActionGroup({
+    source: 'Auth',
+    events: {
+        'Signup': props<{user: Signup}>(),
+        'SignupSuccess': props<{data: SignupResponse}>(),
+        'Login': props<{user: Login}>(),
+        'LoginSuccess': props<{data: LoginResponse}>(),
+        'Verify Account': props<{code: VerifyAccount}>(),
+        'Verify Account Success': props<{message: SuccessResponse}>(),
+        'Auth Error': props<{error: HttpErrorResponse}>(),
+        'Logout': emptyProps()
+    }
+})
