3b9b4c68ca30fcb6598529aba168ad2938a08ccc
george-asiedu <asiedug41@gmail.com>
2025-05-22 12:58:01 +0000

    phone number input implemented with country code

---
+++ src/app/shared/ui/phone-number-input/phone-number-input.component.ts
@@ -0,0 +1,29 @@
+import { Component, input, output } from '@angular/core';
+import { countries } from '../../../constants';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-phone-number-input',
+  imports: [FormsModule],
+  templateUrl: './phone-number-input.component.html',
+  styleUrl: './phone-number-input.component.scss'
+})
+export class PhoneNumberInputComponent {
+  public selectedCountryCode = input('+233');
+  public phoneNumber = input('');
+  public phoneChange = output<string>();
+  public countryChange = output<string>();
+
+  public countries = countries;
+
+  public onCountryChange(code: string) {
+    this.countryChange.emit(code);
+  }
+
+  public onPhoneInput(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    if (inputElement) {
+      this.phoneChange.emit(inputElement.value);
+    }
+  }
+}
