51c6b6372fef909d024ce68e628672a03deb46be
george-asiedu <asiedug41@gmail.com>
2025-05-22 15:27:57 +0000

    auth reducer feature implemented

---
+++ src/app/auth/state/auth.reducers.ts
@@ -0,0 +1,47 @@
+import {createFeature, createReducer, on} from '@ngrx/store';
+import {initialState} from '../../constants';
+import {authActions} from "./auth.actions";
+
+export const authFeature= createFeature({
+    name: 'Auth',
+    reducer: createReducer(
+        initialState,
+        on(authActions.signup, (state) => ({
+            ...state,
+            loading: true,
+        })),
+        on(authActions.login, (state) => ({
+            ...state,
+            loading: true,
+        })),
+        on(authActions.verifyAccount, (state) => ({
+            ...state,
+            loading: true,
+        })),
+        on(authActions.signupSuccess, (state, {data}) => ({
+            ...state,
+            loading: false,
+            data
+        })),
+        on(authActions.loginSuccess, (state, {data}) => ({
+            ...state,
+            loading: false,
+            data
+        })),
+        on(authActions.verifyAccountSuccess, (state, {message}) => ({
+            ...state,
+            loading: false,
+            message
+        })),
+        on(authActions.authError, (state, {error}) => ({
+            ...state,
+            loading: false,
+            error
+        })),
+        on(authActions.logout, (state) => ({
+            ...state,
+            loading: false,
+            data: null
+        }))
+    )
+});
