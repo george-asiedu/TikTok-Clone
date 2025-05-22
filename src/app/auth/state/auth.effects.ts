e0a8407343a04200fbbddd95571d1094f00a6d9d
george-asiedu <asiedug41@gmail.com>
2025-05-22 15:27:42 +0000

    auth effects implemented

---
+++ src/app/auth/state/auth.effects.ts
@@ -0,0 +1,65 @@
+import {Actions, createEffect, ofType} from '@ngrx/effects';
+import {inject} from '@angular/core';
+import {AuthService} from '../service/auth.service';
+import {authActions} from './auth.actions';
+import {filter, switchMap, withLatestFrom} from 'rxjs';
+import {mapResponse} from '@ngrx/operators';
+import {HttpErrorResponse} from '@angular/common/http';
+import {Store} from '@ngrx/store';
+import {AuthState} from '../../interface/auth';
+import {selectSignupToken} from './auth.selector';
+
+export const signupUser = createEffect(
+    (actions$ = inject(Actions),
+     authService = inject(AuthService)) => {
+        return actions$.pipe(
+            ofType(authActions.signup),
+            switchMap(({user}) =>
+                authService.signup(user).pipe(
+                    mapResponse({
+                        next: (response) => authActions.signupSuccess({data: response}),
+                        error: (error: HttpErrorResponse) => authActions.authError({error})
+                    })
+                )
+            )
+        );
+    }, {functional: true, dispatch: true}
+);
+
+export const loginUser = createEffect(
+    (actions$ = inject(Actions),
+     authService = inject(AuthService)) => {
+        return actions$.pipe(
+            ofType(authActions.login),
+            switchMap(({user}) =>
+                authService.login(user).pipe(
+                    mapResponse({
+                        next: (response) => authActions.loginSuccess({data: response}),
+                        error: (error: HttpErrorResponse) => authActions.authError({error})
+                    })
+                )
+            )
+        )
+    }, {functional: true, dispatch: true}
+);
+
+export const verifyUserAccount = createEffect(
+    (actions$ = inject(Actions),
+     authService = inject(AuthService),
+     store = inject(Store<AuthState>)
+    ) => {
+        return actions$.pipe(
+            ofType(authActions.verifyAccount),
+            withLatestFrom(store.select(selectSignupToken)),
+            filter(([, token]) => !!token),
+            switchMap(([{code}, token]) =>
+                authService.verifyAccount(code, token as string).pipe(
+                    mapResponse({
+                        next: (response) => authActions.verifyAccountSuccess({message: response}),
+                        error: (error: HttpErrorResponse) => authActions.authError({error})
+                    })
+                )
+            )
+        )
+    }, {functional: true, dispatch: true}
+);
